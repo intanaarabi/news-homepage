bc69da5a530c1d892fe914e0efea37899e63393d
Dayang Intan <intan.abangarabi@gmail.com>
2023-10-30 17:14:31 +0800

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+    <div 
+      className='
+        md:mx-32
+        md:my-24
+        min-h-screen
+      '>
+        <Navbar/>
+    </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+const navbarLinkClassName = "hover:text-red-500 transition-hover duration-300"
+
+function Navbar() {
+    return (
+        <div 
+        className="
+            w-full
+            flex
+            flex-row
+            justify-between
+            items-center
+        ">
+            <img alt="News Page Icon" src="./images/logo.svg"></img>
+            <div className="flex-grow"></div>
+            <div className="flex flex-row gap-12">
+                <a href="/" className={navbarLinkClassName}>Home</a>
+                <a href="/" className={navbarLinkClassName}>New</a>
+                <a href="/" className={navbarLinkClassName}>Popular</a>
+                <a href="/" className={navbarLinkClassName}>Trending</a>
+                <a href="/" className={navbarLinkClassName}>Categories</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
